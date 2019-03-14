f3efb7665c9120a8b7ba63d3806dbf209cdaa44a
Stephen P. Cook <s.cook@seiche.com>
2019-03-14 12:57:16 +0000

    Initial commit - Lesson 1

---
+++ myscript.py
@@ -0,0 +1,11 @@
+a = 3
+b = list(range(10))
+
+print("Hello world")
+
+def f(x):
+    """Return the product of the input
+    
+    This is "another" line in the function help.
+    """
+    return x*x
